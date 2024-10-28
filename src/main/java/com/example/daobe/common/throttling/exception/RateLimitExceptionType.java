519a3090b573684d00c48e66ca70c5c2fb61f1dd
JiHongKim98 <kinjihong9598@gmail.com>
2024-10-28 21:37:26 +0900

    chore: s3 캐시 시간에 대한 프로퍼티 삭제 #300

--- src/main/java/com/example/daobe/common/throttling/exception/RateLimitExceptionType.java
+++ src/main/java/com/example/daobe/common/throttling/exception/RateLimitExceptionType.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 
 public enum RateLimitExceptionType implements BaseExceptionType {
     THROTTLING_EXCEPTION("잠시후 다시 시도해주세요.", HttpStatus.TOO_MANY_REQUESTS),
+    RATE_LIMIT_USER_ID_ERROR_MESSAGE("사용자 식별자를 가져올 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     ;
 
     private final String message;
