c09a859d48611ccaf94994937f7ca6a2c028b0e9
JiHongKim98 <kinjihong9598@gmail.com>
2024-11-14 22:19:42 +0900

    build: AWS SNS 의존성 추가 #308

--- src/main/java/com/example/daobe/common/outbox/OutboxRepository.java
+++ src/main/java/com/example/daobe/common/outbox/OutboxRepository.java
@@ -1,5 +1,6 @@
 package com.example.daobe.common.outbox;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,8 +8,12 @@ import org.springframework.data.jpa.repository.Query;
 public interface OutboxRepository extends JpaRepository<Outbox, String> {
 
     @Query("""
-            SELECT ob FROM Outbox ob
-            WHERE ob.isComplete = false
+            SELECT o FROM Outbox o
+            WHERE o.createdAt < :time
+                AND o.isComplete = false
+                AND o.deliveryCount < 5
+            ORDER BY o.createdAt ASC
+            LIMIT 100
             """)
-    List<Outbox> findAllByIsCompleteFalse();
+    List<Outbox> findAllBy(LocalDateTime time);
 }
