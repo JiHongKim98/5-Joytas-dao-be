f3b73477ab0d2404f3251ab05d151584246d10fd
JiHongKim98 <kinjihong9598@gmail.com>
2024-11-15 11:29:06 +0900

    refactor: 오브제 초대 이벤트 상속 제거 #302

--- src/main/java/com/example/daobe/objet/domain/event/ObjetInviteEvent.java
+++ src/main/java/com/example/daobe/objet/domain/event/ObjetInviteEvent.java
@@ -1,49 +1,23 @@
 package com.example.daobe.objet.domain.event;
 
-import com.example.daobe.common.domain.DomainEvent;
 import com.example.daobe.objet.domain.ObjetSharer;
 import java.util.UUID;
 
-public class ObjetInviteEvent implements DomainEvent {
+public record ObjetInviteEvent(
+        String eventId,
+        Long senderId,
+        Long receiverId,
+        Long objetId,
+        String objectName
+) {
 
-    private static final String OBJET_NOT_CREATED_EXCEPTION_MESSAGE = "아직 생성되지 않은 오브제 입니다";
-
-    private final String eventId;
-    private final Long domainId;
-    private final Long sendUserId;
-    private final Long receiveUserId;
-
-    public ObjetInviteEvent(Long sendUserId, ObjetSharer objetSharer) {
-        validate(objetSharer);
-        this.eventId = UUID.randomUUID().toString();
-        this.domainId = objetSharer.getObjet().getId();
-        this.sendUserId = sendUserId;
-        this.receiveUserId = objetSharer.getUser().getId();
-    }
-
-    private void validate(ObjetSharer objetSharer) {
-        if (objetSharer.getId() == null) {
-            throw new RuntimeException(OBJET_NOT_CREATED_EXCEPTION_MESSAGE);
-        }
-    }
-
-    @Override
-    public String getEventId() {
-        return eventId;
-    }
-
-    @Override
-    public Long getDomainId() {
-        return domainId;
-    }
-
-    @Override
-    public Long getSendUserId() {
-        return sendUserId;
-    }
-
-    @Override
-    public Long getReceiveUserId() {
-        return receiveUserId;
+    public static ObjetInviteEvent of(Long senderId, ObjetSharer objetSharer) {
+        return new ObjetInviteEvent(
+                UUID.randomUUID().toString(),
+                senderId,
+                objetSharer.getId(),
+                objetSharer.getObjet().getId(),
+                objetSharer.getObjet().getName()
+        );
     }
 }
