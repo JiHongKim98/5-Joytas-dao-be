1cf0f2b26315e40ea6efc251d6507289a2e92974
JiHongKim98 <kinjihong9598@gmail.com>
2024-11-15 12:42:33 +0900

    refactor: 라운지 초대 이벤트 상속 제거 및 이벤트 발행 로직 수정 #302

--- src/main/java/com/example/daobe/lounge/domain/event/LoungeInvitedEvent.java
+++ src/main/java/com/example/daobe/lounge/domain/event/LoungeInvitedEvent.java
@@ -1,52 +1,23 @@
 package com.example.daobe.lounge.domain.event;
 
-import static com.example.daobe.lounge.exception.LoungeExceptionType.LOUNGE_NOT_CREATED_EXCEPTION_MESSAGE;
-
-import com.example.daobe.common.domain.DomainEvent;
-import com.example.daobe.lounge.domain.LoungeSharer;
-import com.example.daobe.lounge.exception.LoungeException;
+import com.example.daobe.lounge.domain.Lounge;
 import java.util.UUID;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(force = true)
-public class LoungeInvitedEvent implements DomainEvent {
+public record LoungeInvitedEvent(
+        String eventId,
+        Long senderId,
+        Long receiverId,
+        Long loungeId,
+        String loungeName
+) {
 
-    private final String eventId;
-    private final Long domainId;
-    private final Long sendUserId;
-    private final Long receiveUserId;
-
-    public LoungeInvitedEvent(Long sendUserId, LoungeSharer loungeSharer) {
-        validate(loungeSharer);
-        this.eventId = UUID.randomUUID().toString();
-        this.domainId = loungeSharer.getLounge().getId();
-        this.sendUserId = sendUserId;
-        this.receiveUserId = loungeSharer.getUser().getId();
-    }
-
-    private void validate(LoungeSharer loungeSharer) {
-        if (loungeSharer.getId() == null) {
-            throw new LoungeException(LOUNGE_NOT_CREATED_EXCEPTION_MESSAGE);
-        }
-    }
-
-    @Override
-    public String getEventId() {
-        return eventId;
-    }
-
-    @Override
-    public Long getDomainId() {
-        return domainId;
-    }
-
-    @Override
-    public Long getSendUserId() {
-        return sendUserId;
-    }
-
-    @Override
-    public Long getReceiveUserId() {
-        return receiveUserId;
+    public static LoungeInvitedEvent of(Long senderId, Long receiverId, Lounge lounge) {
+        return new LoungeInvitedEvent(
+                UUID.randomUUID().toString(),
+                senderId,
+                receiverId,
+                lounge.getId(),
+                lounge.getName()
+        );
     }
 }
