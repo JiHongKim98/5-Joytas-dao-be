d7c1423579a37df7a55304f66330396a09a9b743
JiHongKim98 <kinjihong9598@gmail.com>
2024-11-21 13:43:29 +0900

    refactor: 알림 생성시 이벤트 발행하도록 수정 #307

--- src/main/java/com/example/daobe/notification/application/NotificationCreateService.java
+++ src/main/java/com/example/daobe/notification/application/NotificationCreateService.java
@@ -1,12 +1,13 @@
 package com.example.daobe.notification.application;
 
 import com.example.daobe.notification.application.dto.command.CreateNotificationCommand;
-import com.example.daobe.notification.application.dto.payload.NotificationExternalMessagePayload;
 import com.example.daobe.notification.domain.Notification;
+import com.example.daobe.notification.domain.event.NotificationCreateEvent;
 import com.example.daobe.notification.domain.repository.NotificationRepository;
 import com.example.daobe.user.application.UserService;
 import com.example.daobe.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +18,7 @@ public class NotificationCreateService {
 
     private final UserService userService;
     private final NotificationRepository notificationRepository;
-    private final NotificationExternalEventPublisher externalEventPublisher;
+    private final ApplicationEventPublisher eventPublisher;
 
     public void createNotification(CreateNotificationCommand command) {
         Long userId = command.senderId();
@@ -26,7 +27,7 @@ public class NotificationCreateService {
         Notification notification = command.toNotification(findUser);
         notificationRepository.save(notification);
 
-        NotificationExternalMessagePayload payload = NotificationExternalMessagePayload.of(notification);
-        externalEventPublisher.execute(payload);
+        NotificationCreateEvent event = NotificationCreateEvent.of(notification);
+        eventPublisher.publishEvent(event);
     }
 }
