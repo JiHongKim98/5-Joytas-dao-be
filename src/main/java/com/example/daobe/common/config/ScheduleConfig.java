34c47183f549c20f09a55b6de06848e7aa5f5e02
JiHongKim98 <kinjihong9598@gmail.com>
2024-11-24 01:00:33 +0900

    refactor: 아웃박스를 위한 스케줄러 쓰레드 설정 추가 #309

--- src/main/java/com/example/daobe/common/config/ScheduleConfig.java
+++ src/main/java/com/example/daobe/common/config/ScheduleConfig.java
@@ -7,11 +7,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @Configuration
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "PT10S")
-public class ScheduleConfig {
+public class ScheduleConfig implements SchedulingConfigurer {
+
+    private static final String SCHEDULER_EXECUTOR_NAME_PREFIX = "scheduler-task-";
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(1);
+        taskScheduler.setAwaitTerminationSeconds(5);
+        taskScheduler.setWaitForTasksToCompleteOnShutdown(true);
+        taskScheduler.setThreadNamePrefix(SCHEDULER_EXECUTOR_NAME_PREFIX);
+        taskScheduler.initialize();
+        taskRegistrar.setTaskScheduler(taskScheduler);
+    }
 
     @Bean
     public LockProvider lockProvider(RedisConnectionFactory redisConnectionFactory) {
