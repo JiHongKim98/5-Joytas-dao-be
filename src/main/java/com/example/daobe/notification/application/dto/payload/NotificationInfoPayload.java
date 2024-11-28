93e2bca413ed1457d02dd664129b6c0cbcfde985
JiHongKim98 <kinjihong9598@gmail.com>
2024-11-28 23:17:54 +0900

    refactor: 알림 조회 응답과 알림 전송 페이로드 분리 #307

--- src/main/java/com/example/daobe/notification/application/dto/payload/NotificationInfoPayload.java
+++ src/main/java/com/example/daobe/notification/application/dto/payload/NotificationInfoPayload.java
@@ -1,24 +1,20 @@
-package com.example.daobe.notification.application.dto;
+package com.example.daobe.notification.application.dto.payload;
 
 import com.example.daobe.notification.domain.Notification;
 import com.example.daobe.notification.domain.NotificationDomainInfo;
 import com.example.daobe.user.domain.User;
 
-public record NotificationInfoResponseDto(
+public record NotificationInfoPayload(
         Long notificationId,
         String type,
-        boolean isRead,
-        String dateTime,
         DomainInfo detail,
         SenderUserInfo sender
 ) {
 
-    public static NotificationInfoResponseDto of(Notification notification) {
-        return new NotificationInfoResponseDto(
+    public static NotificationInfoPayload from(Notification notification) {
+        return new NotificationInfoPayload(
                 notification.getId(),
                 notification.getType().type(),
-                notification.isRead(),
-                notification.getCreatedAt().toString(),
                 notification.getDomainInfo() == null ? null : DomainInfo.of(notification.getDomainInfo()),
                 SenderUserInfo.of(notification.getSenderUser())
         );
@@ -26,13 +22,11 @@ public record NotificationInfoResponseDto(
 
     // Nested
     private record DomainInfo(
-            Long domainId,
             String name
     ) {
 
         public static DomainInfo of(NotificationDomainInfo domainInfo) {
             return new DomainInfo(
-                    domainInfo.getDomainId(),
                     domainInfo.getDomainName()
             );
         }
@@ -40,13 +34,11 @@ public record NotificationInfoResponseDto(
 
     // Nested
     private record SenderUserInfo(
-            Long userId,
             String nickname
     ) {
 
         public static SenderUserInfo of(User senderUser) {
             return new SenderUserInfo(
-                    senderUser.getId(),
                     senderUser.getNickname()
             );
         }
