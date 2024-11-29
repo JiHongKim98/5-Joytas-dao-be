67f490d391ea6910045010317d9755f8b1eb5d2a
JiHongKim98 <kinjihong9598@gmail.com>
2024-11-29 10:29:37 +0900

    refactor: 알림 브로드캐스팅 메시지 리스너 로직 수정 #307

--- src/main/java/com/example/daobe/notification/infrastructure/redis/RedisNotificationBroadcastListener.java
+++ src/main/java/com/example/daobe/notification/infrastructure/redis/RedisNotificationBroadcastListener.java
@@ -1,6 +1,6 @@
 package com.example.daobe.notification.infrastructure.redis;
 
-import com.example.daobe.notification.application.NotificationSubscribeService;
+import com.example.daobe.notification.application.NotificationSenderService;
 import com.example.daobe.notification.infrastructure.redis.payload.NotificationCreateEventPayload;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
@@ -11,18 +11,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class RedisNotificationMessageListener implements MessageListener {
+public class RedisNotificationBroadcastListener implements MessageListener {
 
     private final ObjectMapper objectMapper;
-    private final NotificationSubscribeService notificationSubscribeService;
+    private final NotificationSenderService notificationSenderService;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        NotificationCreateEventPayload payload = deserializeFromBytes(message.getBody());
-        notificationSubscribeService.publishToClient(payload.notificationId(), payload.senderId());
+        NotificationCreateEventPayload payload = deserialize(message.getBody());
+        notificationSenderService.sendMessage(payload.notificationId(), payload.senderId());
     }
 
-    private NotificationCreateEventPayload deserializeFromBytes(byte[] bytes) {
+    private NotificationCreateEventPayload deserialize(byte[] bytes) {
         try {
             return objectMapper.readValue(bytes, NotificationCreateEventPayload.class);
         } catch (IOException e) {
