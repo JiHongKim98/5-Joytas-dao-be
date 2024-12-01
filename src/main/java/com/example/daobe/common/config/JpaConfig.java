ff6cc149cb0668aa997aa6f87fd25c8ac3123ac0
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-01 11:33:52 +0900

    refactor: JPA 패키지 스캔 범위 설정 #313

--- src/main/java/com/example/daobe/common/config/JpaConfig.java
+++ src/main/java/com/example/daobe/common/config/JpaConfig.java
@@ -1,9 +1,22 @@
 package com.example.daobe.common.config;
 
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(
+        basePackages = {
+                "com.example.daobe.common.outbox",
+                "com.example.daobe.*.domain.repository",
+        },
+        excludeFilters = {
+                @Filter(type = FilterType.ASSIGNABLE_TYPE, value = MongoRepository.class),
+        }
+)
 public class JpaConfig {
 }
