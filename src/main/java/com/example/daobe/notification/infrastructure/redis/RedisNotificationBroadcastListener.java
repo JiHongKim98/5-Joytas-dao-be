bcf8fac2530124c272205c654298093e4c5cb166
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-02 13:38:42 +0900

    refactor: 알림 브로드캐스트 리스너 로직 수정 #312

--- src/main/java/com/example/daobe/notification/infrastructure/redis/RedisNotificationBroadcastListener.java
+++ src/main/java/com/example/daobe/notification/infrastructure/redis/RedisNotificationBroadcastListener.java
@@ -3,28 +3,27 @@ package com.example.daobe.notification.infrastructure.redis;
 import com.example.daobe.notification.application.NotificationSenderService;
 import com.example.daobe.notification.infrastructure.redis.payload.NotificationCreateEventPayload;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.jihongkim98.redisextensions.annotation.RedisBroadcastListener;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class RedisNotificationBroadcastListener implements MessageListener {
+public class RedisNotificationBroadcastListener {
 
     private final ObjectMapper objectMapper;
     private final NotificationSenderService notificationSenderService;
 
-    @Override
-    public void onMessage(Message message, byte[] pattern) {
-        NotificationCreateEventPayload payload = deserialize(message.getBody());
+    @RedisBroadcastListener(channels = "channel:notification")
+    public void notificationBroadcastListener(String message) {
+        NotificationCreateEventPayload payload = deserialize(message);
         notificationSenderService.sendMessage(payload.notificationId(), payload.senderId());
     }
 
-    private NotificationCreateEventPayload deserialize(byte[] bytes) {
+    private NotificationCreateEventPayload deserialize(String message) {
         try {
-            return objectMapper.readValue(bytes, NotificationCreateEventPayload.class);
+            return objectMapper.readValue(message, NotificationCreateEventPayload.class);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
