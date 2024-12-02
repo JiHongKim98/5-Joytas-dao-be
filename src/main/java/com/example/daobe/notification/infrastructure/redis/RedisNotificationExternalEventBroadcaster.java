18356aa7660f37a8da07661be4894230b03eed99
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-02 13:22:07 +0900

    refactor: 알림 브로드캐스트 이벤트 발행 로직 리팩토링 #312

--- src/main/java/com/example/daobe/notification/infrastructure/redis/RedisNotificationExternalEventBroadcaster.java
+++ src/main/java/com/example/daobe/notification/infrastructure/redis/RedisNotificationExternalEventBroadcaster.java
@@ -3,37 +3,21 @@ package com.example.daobe.notification.infrastructure.redis;
 import com.example.daobe.notification.application.NotificationExternalEventBroadcaster;
 import com.example.daobe.notification.domain.event.NotificationCreateEvent;
 import com.example.daobe.notification.infrastructure.redis.payload.NotificationCreateEventPayload;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RedisNotificationExternalEventBroadcaster implements NotificationExternalEventBroadcaster {
 
-    private static final String NOTIFICATION_CHANNEL_TOPIC = "notification:channel";
+    private static final String NOTIFICATION_CHANNEL_TOPIC = "channel:notification";
 
-    private final ObjectMapper objectMapper;
-    private final StringRedisTemplate redisTemplate;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Override
     public void execute(NotificationCreateEvent event) {
         NotificationCreateEventPayload payload = NotificationCreateEventPayload.from(event);
-        redisTemplate.convertAndSend(
-                NOTIFICATION_CHANNEL_TOPIC,
-                serialize(payload)
-        );
-    }
-
-    private String serialize(NotificationCreateEventPayload payload) {
-        try {
-            return objectMapper.writeValueAsString(payload);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        redisTemplate.convertAndSend(NOTIFICATION_CHANNEL_TOPIC, payload);
     }
 }
