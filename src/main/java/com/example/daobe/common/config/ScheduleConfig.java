7074fdeaf9e08dd68dabc8011386ba5f187362e3
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-03 18:23:05 +0900

    refactor: 스케쥴러 라이프사이클 관리 설정 적용 #314

--- src/main/java/com/example/daobe/common/config/ScheduleConfig.java
+++ src/main/java/com/example/daobe/common/config/ScheduleConfig.java
@@ -3,6 +3,7 @@ package com.example.daobe.common.config;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.SmartLifecycle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,23 +15,43 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 @Configuration
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "PT10S")
-public class ScheduleConfig implements SchedulingConfigurer {
+public class ScheduleConfig implements SchedulingConfigurer, SmartLifecycle {
 
     private static final String SCHEDULER_EXECUTOR_NAME_PREFIX = "scheduler-task-";
 
-    @Override
-    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+    private final ThreadPoolTaskScheduler taskScheduler = createTaskSchedulerThreadPool();
+
+    private ThreadPoolTaskScheduler createTaskSchedulerThreadPool() {
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
         taskScheduler.setPoolSize(1);
         taskScheduler.setAwaitTerminationSeconds(5);
         taskScheduler.setWaitForTasksToCompleteOnShutdown(true);
         taskScheduler.setThreadNamePrefix(SCHEDULER_EXECUTOR_NAME_PREFIX);
-        taskScheduler.initialize();
-        taskRegistrar.setTaskScheduler(taskScheduler);
+        return taskScheduler;
     }
 
     @Bean
     public LockProvider lockProvider(RedisConnectionFactory redisConnectionFactory) {
         return new RedisLockProvider(redisConnectionFactory);
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskScheduler);
+    }
+
+    @Override
+    public void start() {
+        taskScheduler.initialize();
+    }
+
+    @Override
+    public void stop() {
+        taskScheduler.shutdown();
+    }
+
+    @Override
+    public boolean isRunning() {
+        return taskScheduler.isRunning();
     }
 }
